c46bd7bbd0dc9393f3a6622099fc2d538af5ad0f
samaksh Jain <samakshjain@live.com>
2016-04-17 01:10:34 +0530

    added tests

--- music/tests.py
+++ music/tests.py
@@ -1,3 +1,34 @@
 from django.test import TestCase
-
+from models import Album, Song
 # Create your tests here.
+
+
+class AlbumTestCase(TestCase):
+    def setUp(self):
+        Album.objects.create(
+            artist="GuitarGod",
+            album_title="EpicTitle",
+            genre="ROCK",
+            album_art="somealbumart.png")
+
+    def test_album_created(self):
+        EpicAlbum = Album.objects.get(album_title="EpicTitle")
+        self.assertEqual(str(EpicAlbum), "EpicTitle by GuitarGod")
+
+
+class SongTestCase(TestCase):
+    def setUp(self):
+        Album.objects.create(
+            artist="GuitarGod",
+            album_title="EpicTitle",
+            genre="ROCK",
+            album_art="somealbumart.png")
+
+        Song.objects.create(
+            song_title="RockAnthem",
+            file_type="mp3",
+            album=Album.objects.get(album_title="EpicTitle"))
+
+    def test_song_creted(self):
+        RockAnthem = Song.objects.get(song_title="RockAnthem")
+        self.assertEqual(str(RockAnthem), "RockAnthem.mp3")
