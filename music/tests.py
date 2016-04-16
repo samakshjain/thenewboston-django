fc9a75d3164d08c3b05a8ca3eff71f91cc74010b
samaksh Jain <samakshjain@live.com>
2016-04-17 01:35:38 +0530

    added get request tests

--- music/tests.py
+++ music/tests.py
@@ -1,22 +1,22 @@
-from django.test import TestCase
+from django.test import TestCase, Client
 from music.models import Album, Song
 # Create your tests here.
 
 
-class AlbumTestCase(TestCase):
-    def setUp(self):
-        Album.objects.create(
-            artist="GuitarGod",
-            album_title="EpicTitle",
-            genre="ROCK",
-            album_art="somealbumart.png")
+# class AlbumTestCase(TestCase):
+#     def setUp(self):
+#         Album.objects.create(
+#             artist="GuitarGod",
+#             album_title="EpicTitle",
+#             genre="ROCK",
+#             album_art="somealbumart.png")
 
-    def test_album_created(self):
-        EpicAlbum = Album.objects.get(album_title="EpicTitle")
-        self.assertEqual(str(EpicAlbum), "EpicTitle by GuitarGod")
+#     def test_album_created(self):
+#         EpicAlbum = Album.objects.get(album_title="EpicTitle")
+#         self.assertEqual(str(EpicAlbum), "EpicTitle by GuitarGod")
 
 
-class SongTestCase(TestCase):
+class modelCreateTests(TestCase):
     def setUp(self):
         Album.objects.create(
             artist="GuitarGod",
@@ -29,6 +29,31 @@ class SongTestCase(TestCase):
             file_type="mp3",
             album=Album.objects.get(album_title="EpicTitle"))
 
+    def test_album_created(self):
+        EpicAlbum = Album.objects.get(album_title="EpicTitle")
+        self.assertEqual(str(EpicAlbum), "EpicTitle by GuitarGod")
+
     def test_song_creted(self):
         RockAnthem = Song.objects.get(song_title="RockAnthem")
         self.assertEqual(str(RockAnthem), "RockAnthem.mp3")
+
+
+class urlTests(TestCase):
+    def setUp(self):
+        Album.objects.create(
+            artist="GuitarGod",
+            album_title="EpicTitle",
+            genre="ROCK",
+            album_art="somealbumart.png")
+
+        Song.objects.create(
+            song_title="RockAnthem",
+            file_type="mp3",
+            album=Album.objects.get(album_title="EpicTitle"))
+        self.c = Client()
+
+    def test_get_requests(self):
+        music_response = self.c.get('/music/')
+        album_response = self.c.get('/music/1/')
+        self.assertEqual(music_response.status_code, 200)
+        self.assertEqual(album_response.status_code, 200)
